6bc4935aaeada166d812444160e536cfa11c81d8
Romanchu <maro.pereyra@alumno.etec.um.edu.ar>
2024-10-24 11:39:46 -0300

    terminado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'; 
-import logo from '../../multimedia/Logomdi.png'
+import logo from '../../multimedia/Logomdi.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Alterna el estado del menú
+  };
+
   return (
     <header className="header">
-      <img src= {logo} alt="Logo de La Duplicadora" className="logo" />
-      <nav className="nav">
-        <li class="item"><Link to="/" class="link">Inicio</Link> </li>
-        <li class="item"><Link to="/personalizacion" class="link">Pedidos</Link></li>
-        <li class="item"><Link to="/registro" class="link">Registrarse</Link></li>
-        <li class="item"><Link to="/login" class="link">Iniciar Sesión</Link></li>
-        
+      <img src={logo} alt="Logo de La Duplicadora" className="logo" />
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰ {/* Este es el ícono de hamburguesa */}
+      </button>
+
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}> 
+        <li className="item"><Link to="/" className="link">Inicio</Link></li>
+        <li className="item"><Link to="/personalizacion" className="link">Pedidos</Link></li>
+        <li className="item"><Link to="/registro" className="link">Registrarse</Link></li>
+        <li className="item"><Link to="/login" className="link">Iniciar Sesión</Link></li>
       </nav>
     </header>
   );
